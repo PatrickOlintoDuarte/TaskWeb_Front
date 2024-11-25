590680a80c65482741186e907044431a3dca3356
Patrick <100382449+PatrickOlintoDuarte@users.noreply.github.com>
2024-11-25 12:41:43 -0300

    Add files via upload

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -1,19 +1,18 @@
+
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Login from './Componentes/auth/Login';
 import TelaAtividade from '../TelaAtividade';
 import TelaGit from '../Telas/UploadGit/TelaGit';
 import ConsultarMetricas from '../Telas/ConsultarMetricas/ConsultarMetricas';
 import GerenciarAnalista from '../Telas/GerenciarAnalista/GerenciarAnalista';
 import TelaAdmin from '../TelaAdmin/TelaAdmin';
-import Teste from './Teste'; // Import da tela Teste
-import TelaInicial from './TelaInicial'; // Import da tela TelaInicial
 
 const Routes = () => {
     return (
         <Router>
-            <Route path='/teste' component={Teste}/> {/* Mantendo a tela Teste */}
-            <Route path='/login' component={Login}/>
+            <Route path='/teste' component={Teste}/>
+            <Route path='/' component={Login}/>
             <Route path='/telainicial' component={TelaInicial}/>
             <Route path='/telaatividade' component={TelaAtividade}/>
             <Route path='/telagit' component={TelaGit}/>
@@ -25,3 +24,4 @@ const Routes = () => {
 };
 
 export default Routes;
+
